898c0880dc921f6778460f244ab60965ed7091ad
stofife <stevondub@gmail.com>
2020-12-31 21:16:36 +0100

    Added FileCount() - returns number of files in a folder

--- main.py
+++ main.py
@@ -40,15 +40,21 @@ def DownloadImages(subreddit_name: str, amount: int, folder: str = "reddit", ext
             r = requests.get(url)
             with open(os.path.join(path,str(count)+".png"), "wb") as f:
                 f.write(r.content)
- 
+
+
 def cwd(): return os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+
+def FileCount(folder):
+    f = cwd() + "\\" + folder
+    return len([name for name in os.listdir(f) if os.path.isfile(f + '\\' + name)]) + 1
+
 
 def DeleteImages(folder):
 
     loc = cwd() + "\\" + folder
 
-    amount = len([name for name in os.listdir(loc) if os.path.isfile(loc + '\\' + name)]) + 1
-
+    amount = FileCount(folder)
     # Uncomment this when debugging ;3
     # print("Deleting the contents of: " + folder)
 
@@ -59,7 +65,7 @@ def DeleteImages(folder):
             #print("Successfully deleted: " + str(file) + ".png")
 
 
-def ResizeImage(name, nw, nh, folder, newfolder = "resized"):
+def ResizeImage(name: str, nw, nh, folder, newfolder = "resized"):
 
     if not os.path.exists(cwd() + "\\" + newfolder):
         os.makedirs(cwd() + "\\" + newfolder)
@@ -69,28 +75,60 @@ def ResizeImage(name, nw, nh, folder, newfolder = "resized"):
     img = img.resize((nw, nh), Image.BICUBIC)
 
     img.save(cwd() + "\\" + newfolder + "\\" + name + ".png")
+
     # Uncomment this when debugging ;3
-    #print("Image was resized and saved to: " + cwd() + "\\" + newfolder)
+    print("Image was resized and saved to: " + cwd() + "\\" + newfolder + "\\" + name + ".png")
 
 
 def RGBtoArray(name: str, folder = "resized"):
 
     img = Image.open(cwd() + "\\" + folder + "\\" + name + ".png")
 
-    return np.asarray(img)
+    return np.asarray(img).transpose(2,0,1) 
 
 
-def GenerateFeatureMap(array, is3d = False):
-    if is3d != False:
-        slice = array[:, :, 1]
-        print(slice.shape)
+def SliceArray(array, dimnum):
+
+    return array[dimnum, :, :]
 
 
-ResizeImage(str(1), 120, 90, "samples")
-w = RGBtoArray(str(1), "resized")
-print(w.shape)
-GenerateFeatureMap(w, True)
-#DownloadImages("furry", 10)
-#for i in range(10):
-#   ResizeImage(str(i+1), 120, 90, "reddit")
-#DeleteImages("reddit")
+def PixelPooling(array, pooldim: int, option: str = "AVERAGE"):
+
+    def avgpool(array, pooldim: int):
+        #array must be 2D
+        newarray = np.zeros((array.shape[0]//pooldim, array.shape[0]//pooldim), int)
+        result = 0
+        for column in range(array.shape[0]//pooldim):
+            for r in range(array.shape[0]//pooldim):
+                for a in range(pooldim):
+                    for v in range(pooldim):
+                        result += array[a + column * pooldim][v + r * pooldim]
+
+                avg = result//(pooldim*pooldim)
+
+                newarray[column][r] = avg
+
+                result = 0
+                
+        return newarray
+
+    if option == "AVERAGE":
+        output = []
+        for dimension in range(array.shape[0]):
+            s = SliceArray(array, dimension)
+            output.append(avgpool(s, pooldim))
+        return np.asarray(output)
+
+
+folder = "yiff"
+
+#DownloadImages("yiff", 20, folder)
+
+#for file in range(1, FileCount(folder)):
+for file in range(1, 2):
+    ResizeImage(str(file), 120, 120, folder)
+    colorarray = RGBtoArray(str(file))
+    print(colorarray.shape)
+    pooled = PixelPooling(colorarray, 3)
+    print(pooled.shape)
+
